d7b05e22fc4670d35caccc23a09a373988fb6665
Guillaume Théberge-Dupuis <theg2902@usherbrooke.ca>
2019-11-21 11:29:51 -0500

    Vitesse par trap et simpson ainsi que hauteur selon trap et simspson

--- App6Problematique.m
+++ App6Problematique.m
@@ -21,6 +21,8 @@ V_IniR       = 6100;        % Vitesse initiale expérience des russes en m/s
 Gamma_IniR   = -90;         % Angle gamma initial expérience des russes en °
 H_IniR       = 120000;      % Hauteur initiale expérience des russes en m
 Theta_IniR   = -90;         % Angle theta initial expérience des russes en °
+Br_Mes_AccR  = 0.03;        % Bruit sur les mesur de l'accéléromètre des russes
+P_BalR       = 0.0192;      % Paramètre balistique des russe en m^2/kg 
 V_IniM       = 6100;        % Vitesse initiale du mandat en m/s
 Gamma_IniM   = -20.5;       % Angle gamma initial du mandat en °
 H_IniM       = 120000;      % Hauteur initiale du mandat en m
@@ -30,32 +32,72 @@ V_FinM2      = 300;         % Vitesse maximale finale du mandat en m/s
 H_FinM       = 10000;       % Hauteur du déploiment des parachutes (h finale) en m
 T_Lim        = 45;          % Temps limite pour une force sur la capsule de plus de 2000N en s
 Theta_cmdLim = 60;          % Angle maximale en absolue de la capsule en °
+
 %% Détermination des paramètre Hs et Po
 H_V_Trap = Temps(2)-Temps(1);
+
 % Détermination de la vitesse selon l'accélération par méthode des trapèze
 DpAccMes_a = (Acc_mes(2)-Acc_mes(1))/H_V_Trap;
 DpAccMes_b = (Acc_mes(end)- Acc_mes(end-1))/H_V_Trap;
 
-V_Mes_Trap   = H_V_Trap/2*(Acc_mes(1)+Acc_mes(end)+2*sum(Acc_mes(2:end-1)));
-E_V_Mes_Trap = H_V_Trap^2/12*(DpAccMes_b-DpAccMes_a);
+V_Mes_Trap   = H_V_Trap/2*(Acc_mes(1)+Acc_mes(end)+2*sum(Acc_mes(2:end-1)))+V_IniM;
+E_V_Mes_Trap = -H_V_Trap^2/12*(DpAccMes_b-DpAccMes_a);
 
 %intégrale point par point
-V_Mes_Trap_Aff(length(Temps))=0;
+V_Mes_Trap_Vec(1)=V_IniM;
 for n=2:1:length(Temps)
-    V_Mes_Trap_Aff(n) = H_V_Trap/2*(Acc_mes(1)+Acc_mes(n)+2*sum(Acc_mes(2:n-1)));
+    V_Mes_Trap_Vec(n) = H_V_Trap/2*(Acc_mes(1)+Acc_mes(n)+2*sum(Acc_mes(2:n-1)))+V_IniM;
 end
-disp(V_Mes_Trap_Aff(11))
+
+
 % Détermination de la vitesse selon l'accélération par méthode de Simpson
 DtAccMes_a = (Acc_mes(4)-3*Acc_mes(3)+3*Acc_mes(2)-Acc_mes(1))/H_V_Trap^3;
-DtAccMes_b = (y(end)-3*y(end-1)+3*y(end-2)-y(end-3))/H_V_Trap^3;
-int_s = H_V_Trap/3*(y(1)+y(end)+4*sum(y(2:2:end-1))+2*sum(y(3:2:end-1)));
-erreur_simp = -H_V_Trap^4/180*(y(end)-y(1))*(DtAccMes_b-DtAccMes_a);
+DtAccMes_b = (Acc_mes(end)-3*Acc_mes(end-1)+3*Acc_mes(end-2)-Acc_mes(end-3))/H_V_Trap^3;
 
-F(1)=0;
+V_Mes_Simp = H_V_Trap/3*(Acc_mes(1)+Acc_mes(end)+4*sum(Acc_mes(2:2:end-1))+2*sum(Acc_mes(3:2:end-1)))+V_IniM;
+E_V_Mes_Simp = -H_V_Trap^4/180*(Acc_mes(end)-Acc_mes(1))*(DtAccMes_b-DtAccMes_a);
+
+V_Mes_Simp_Aff(1)=V_IniM;
 for n=3:2:length(Temps)
-    F(((n-1)/2)+1) = H_V_Trap/3*(y(1)+y(n)+4*sum(y(2:2:n-1))+2*sum(y(3:2:n-1)));
+    V_Mes_Simp_Aff(((n-1)/2)+1) = H_V_Trap/3*(Acc_mes(1)+Acc_mes(n)+4*sum(Acc_mes(2:2:n-1))+2*sum(Acc_mes(3:2:n-1)))+V_IniM;
+end
+figure()
+plot (Temps(1:2:end),V_Mes_Simp_Aff)
+hold on
+plot(Temps,V_Mes_Trap_Vec);
+title('Vitesse en fonction du temps selon différente approximation')
+legend('Simpson','Trapèze')
+% La méthode des trapèze est sélectionnée car sont erreure est plus petite
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Détermination de la hauteur selon la vitesse
+DpVmes_a = (V_Mes_Trap_Vec(2)-V_Mes_Trap_Vec(1))/H_V_Trap;
+DpVmes_b = (V_Mes_Trap_Vec(end)- V_Mes_Trap_Vec(end-1))/H_V_Trap;
+
+H_Mes_Trap   = H_IniM - H_V_Trap/2*(V_Mes_Trap_Vec(1)+V_Mes_Trap_Vec(end)+2*sum(V_Mes_Trap_Vec(2:end-1)));
+E_H_Mes_Trap = -H_V_Trap^2/12*(DpVmes_b-DpVmes_a)
+
+%intégrale point par point
+H_Mes_Trap_Aff(1)=H_IniM;
+for n=2:1:length(Temps)
+    H_Mes_Trap_Aff(n) = H_IniM - H_V_Trap/2*(V_Mes_Trap_Vec(1)+V_Mes_Trap_Vec(n)+2*sum(V_Mes_Trap_Vec(2:n-1)));
 end
 
-plot (Temps(1:2:end),F)
+% Détermination de la vitesse selon l'accélération par méthode de Simpson
+DtHmes_a = (V_Mes_Trap_Vec(4)-3*V_Mes_Trap_Vec(3)+3*V_Mes_Trap_Vec(2)-V_Mes_Trap_Vec(1))/H_V_Trap^3;
+DtHmes_b = (V_Mes_Trap_Vec(end)-3*V_Mes_Trap_Vec(end-1)+3*V_Mes_Trap_Vec(end-2)-V_Mes_Trap_Vec(end-3))/H_V_Trap^3;
+
+H_Mes_Simp = H_IniM - H_V_Trap/3*(V_Mes_Trap_Vec(1)+V_Mes_Trap_Vec(end)+4*sum(V_Mes_Trap_Vec(2:2:end-1))+2*sum(V_Mes_Trap_Vec(3:2:end-1)));
+E_H_Mes_Simp = -H_V_Trap^4/180*(V_Mes_Trap_Vec(end)-V_Mes_Trap_Vec(1))*(DtHmes_b-DtHmes_a)
+
+H_Mes_Simp_Aff(1)=H_IniM;
+for n=3:2:length(Temps)
+    H_Mes_Simp_Aff(((n-1)/2)+1) = H_IniM - H_V_Trap/3*(V_Mes_Trap_Vec(1)+V_Mes_Trap_Vec(n)+4*sum(V_Mes_Trap_Vec(2:2:n-1))+2*sum(V_Mes_Trap_Vec(3:2:n-1)));
+end
+figure()
+plot (Temps(1:2:end),H_Mes_Simp_Aff)
 hold on
-plot(Temps,V_Mes_Trap_Aff);
+plot(Temps,H_Mes_Trap_Aff);
+% title('Hauteur en fonction du temps selon différente approximation')
+legend('Simpson','Trapèze')
