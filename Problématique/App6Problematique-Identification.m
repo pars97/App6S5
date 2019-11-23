373b942a68f3e4e78ae98b072403f960e0ea85a3
arsp2701 <arsp2701>
2019-11-22 19:57:22 -0500

    Pb avancée

--- Problématique/App6Problematique-Identification.m
+++ Problématique/App6Problematique-Identification.m
@@ -1,11 +1,11 @@
 close all
 clear all
 clc
-
+addpath ../
 load('Accelero_Data_from_Moscow');
 %% Définition paramètres et données connues
 Temps        = t;           % Temps des données des russes en s
-Acc_mes      = -acc_mes;     % Données des russes en m/s^2
+Acc_mes      = -acc_mes;    % Données des russes en m/s^2
 M_c          = 50;          % Masse de la capsule en kg
 J_c          = 1.5;         % Inertie de la capsule en kg-m^2
 R_mars       = 3397*10^3;   % Rayon de mars en m
@@ -40,11 +40,14 @@ H_V_Trap = Temps(2)-Temps(1);
 DpAccMes_a = (Acc_mes(2)-Acc_mes(1))/H_V_Trap;
 DpAccMes_b = (Acc_mes(end)- Acc_mes(end-1))/H_V_Trap;
 
+
 V_Mes_Trap   = H_V_Trap/2*(Acc_mes(1)+Acc_mes(end)+2*sum(Acc_mes(2:end-1)))+V_IniR;
 E_V_Mes_Trap = -H_V_Trap^2/12*(DpAccMes_b-DpAccMes_a);
 
 %intégrale point par point
 V_Mes_Trap_Vec(1)=V_IniR;
+
+
 for n=2:1:length(Temps)
     V_Mes_Trap_Vec(n) = H_V_Trap/2*(Acc_mes(1)+Acc_mes(n)+2*sum(Acc_mes(2:n-1)))+V_IniR;
 end
@@ -58,15 +61,21 @@ V_Mes_Simp = H_V_Trap/3*(Acc_mes(1)+Acc_mes(end)+4*sum(Acc_mes(2:2:end-1))+2*sum
 E_V_Mes_Simp = -H_V_Trap^4/180*(Acc_mes(end)-Acc_mes(1))*(DtAccMes_b-DtAccMes_a);
 
 V_Mes_Simp_Vec(1)=V_IniR;
+
+
+
 for n=3:2:length(Temps)
     V_Mes_Simp_Vec(((n-1)/2)+1) = H_V_Trap/3*(Acc_mes(1)+Acc_mes(n)+4*sum(Acc_mes(2:2:n-1))+2*sum(Acc_mes(3:2:n-1)))+V_IniR;
 end
-figure()
-plot (Temps(1:2:end),V_Mes_Simp_Vec)
-hold on
-plot(Temps,V_Mes_Trap_Vec);
-title('Vitesse en fonction du temps selon différente approximation')
-legend('Simpson','Trapèze')
+
+
+
+% figure()
+% plot (Temps(1:2:end),V_Mes_Simp_Vec)
+% hold on
+% plot(Temps,V_Mes_Trap_Vec);
+% title('Vitesse en fonction du temps selon différente approximation')
+% legend('Simpson','Trapèze')
 % La méthode des trapèze est sélectionnée car sont erreure est plus petite
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -80,6 +89,8 @@ E_H_Mes_Trap = H_V_Trap^2/12*(DpVmes_b-DpVmes_a)
 
 %intégrale point par point
 H_Mes_Trap_Vec(1)=H_IniR;
+
+
 for n=2:1:length(Temps)
     H_Mes_Trap_Vec(n) = H_IniR - H_V_Trap/2*(V_Mes_Trap_Vec(1)+V_Mes_Trap_Vec(n)+2*sum(V_Mes_Trap_Vec(2:n-1)));
 end
@@ -92,25 +103,82 @@ H_Mes_Simp = H_IniR + H_V_Trap/3*(V_Mes_Trap_Vec(1)+V_Mes_Trap_Vec(end)+4*sum(V_
 E_H_Mes_Simp = -H_V_Trap^4/180*(V_Mes_Trap_Vec(end)-V_Mes_Trap_Vec(1))*(DtHmes_b-DtHmes_a)
 
 H_Mes_Simp_Aff(1)=H_IniR;
+
+
 for n=3:2:length(Temps)
     H_Mes_Simp_Aff(((n-1)/2)+1) = H_IniR - H_V_Trap/3*(V_Mes_Trap_Vec(1)+V_Mes_Trap_Vec(n)+4*sum(V_Mes_Trap_Vec(2:2:n-1))+2*sum(V_Mes_Trap_Vec(3:2:n-1)));
 end
-figure()
-plot (Temps(1:2:end),H_Mes_Simp_Aff)
-hold on
-plot(Temps,H_Mes_Trap_Vec);
-% title('Hauteur en fonction du temps selon différente approximation')
-legend('Simpson','Trapèze')
-% La méthode des trapèze est sélectionnée car sont erreure est plus petite
 
-%% identiification des parametre
 
-Y = log((acc_mes'*2*M_c)./(V_Mes_Trap_Vec.^2*S_c*C_do));
+% figure()
+% plot (Temps(1:2:end),H_Mes_Simp_Aff)
+% hold on
+% plot(Temps,H_Mes_Trap_Vec);
+% % title('Hauteur en fonction du temps selon différente approximation')
+% legend('Simpson','Trapèze')
+% La méthode des trapèzes est sélectionnée car sont erreure est plus petite
+
+%% Identification des paramètres et RMS/R2 linéaire
+
+Rho_mes = (acc_mes'*2*M_c)./(V_Mes_Trap_Vec.^2*S_c*C_do);
+Y = log(Rho_mes);
 X = H_Mes_Trap_Vec;
 N = length(X);
 
 param = inv([N sum(X); sum(X) sum(X.^2)])*[sum(Y);sum(Y.*X)];
 
+
 H_s = -1/param(2);
 Rho_0 = exp(param(1));
+
+
+Ys = param(2)*X+param(1);
+
+
+RMS_lin = sqrt(1/N*sum((Ys-Y).^2));
+R2_lin = sum((Ys-mean(Y)).^2)/sum((Y-mean(Y)).^2);
+
+Rho_approx = Rho_0*exp(-H_Mes_Trap_Vec/H_s);
+
+Acc_approx = (1/2.*Rho_approx.*V_Mes_Trap_Vec.^2*S_c*C_do/M_c)';
+
+% RMS de l'approximation au niveau de l'accélération
+
+RMS_acc_rel = sqrt(1/N*sum(((acc_mes-Acc_approx)).^2))
+
+RMS_acc_rel = sqrt(1/N*sum(((acc_mes-Acc_approx)./acc_mes).^2))
+
+% plot(H_Mes_Trap_Vec,[acc_mes,Acc_approx])
+
+%% Loi de guidage
+%--------------- A partir de cette ligne en développement-----------------%
+%H_FinM/V_FinM1/V_FinM2
+
+
+Delta_V_aero = V_FinM1 - sqrt(V_Mes_Trap_Vec.^2+2*Mu_mars*(1/(R_mars+H_FinM)-1./(R_mars+H_Mes_Trap_Vec)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
